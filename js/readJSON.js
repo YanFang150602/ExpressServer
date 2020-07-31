7d89e268c97b5e72408f5b03221d2bf11ef08e91
颜泽芳 <838641570@qq.com>
2020-07-31 14:06:21 +0800

    Update readJSON.js

--- js/readJSON.js
+++ js/readJSON.js
@@ -1,1 +1,23 @@
-vmconst
+const fs = require('fs');
+const path = require('path');
+
+const getFilename = (url) => {
+    return url.replace(new RegExp('/','g'), '')
+              .replace(/:/g, '') + '.json';
+}
+
+const readFileSync = (file) => {
+    console.log('readFileSync:' + file);
+    const data = fs.readFileSync(file);
+    return JSON.parse(data.toString());
+}
+
+const getJSONData = (url) => {
+    let filename = getFilename(url);
+    let file = path.join(process.cwd(), 'data', filename);
+    return readFileSync(file);
+}
+
+module.exports = {
+    readData: getJSONData
+}
